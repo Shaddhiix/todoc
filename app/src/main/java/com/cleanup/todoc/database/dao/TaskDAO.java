50e5277bd0125067bdec85e1f95aa56aa24d862f
Shaddhiix <leonard.alexandre17@gmail.com>
2022-07-20 12:49:32 +0200

    Mise en place des "DAO"

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDAO.java
@@ -0,0 +1,26 @@
+package com.cleanup.todoc.database.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDAO {
+    
+    @Query ( "SELECT * FROM Task WHERE projectId = :projectId" )
+    LiveData<List<Task>> getTasks(long projectId);
+    
+    @Insert
+    long insertTask(Task task);
+    
+    @Insert
+    int updateTask(Task task);
+    
+    @Query ( "DELETE FROM Task WHERE id = :taskId" )
+    int deleteTask(long taskId);
+}
